0b6a4bd4a1adf85cfbbfe4160064db27ab0dd50c
Amr Ashraf <AmrAshraf_inf@outlook.com>
2026-09-08 22:22:24 +0300

    feat: add leave management entities and API integration

---
+++ lib/features/leave/domain/repository/leave_repository.dart
@@ -0,0 +1,25 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../../../../core/domain/failure/domain_failure.dart';
+import '../entity/leave_balance.dart';
+import '../entity/leave_request.dart';
+import '../entity/leave_status.dart';
+import '../entity/leave_type.dart';
+
+abstract class LeaveRepository {
+  Future<Either<Failure, List<LeaveType>>> getLeaveTypes();
+
+  Future<Either<Failure, List<LeaveRequest>>> getMyLeaveRequests({LeaveStatus? status});
+
+  Future<Either<Failure, LeaveRequest>> submitLeaveRequest({
+    required int leaveTypeId,
+    required DateTime startDate,
+    required DateTime endDate,
+    required String reason,
+    String? attachmentPath,
+  });
+
+  Future<Either<Failure, LeaveBalanceSummary>> getLeaveBalance({int? year});
+
+  Future<Either<Failure, Unit>> cancelLeaveRequest(int id);
+}
